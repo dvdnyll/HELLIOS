24a98f59db46f3e352a66d10d567f42c22cdc3f4
Bhre. <109359894+dvdnyll@users.noreply.github.com>
2026-04-26 15:08:05 +0700

    Update smartmenu.js

--- plugins/smartmenu.js
+++ plugins/smartmenu.js
@@ -117,7 +117,7 @@ export default {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363319098372999@newsletter',
-                    newsletterName: 'GlobalTechInc',
+                    newsletterName: 'HELLIOS',
                     serverMessageId: -1
                 }
             };
