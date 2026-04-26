b1d0c3c60589f91dda8f421d3588133d16c5f089
Bhre. <109359894+dvdnyll@users.noreply.github.com>
2026-04-26 15:02:18 +0700

    Change newsletter name from 'GlobalTechInc' to 'HELLIOS'

--- index.js
+++ index.js
@@ -293,7 +293,7 @@ async function startQasimDev() {
                                 isForwarded: true,
                                 forwardedNewsletterMessageInfo: {
                                     newsletterJid: '120363319098372999@newsletter',
-                                    newsletterName: 'GlobalTechInc',
+                                    newsletterName: 'HELLIOS',
                                     serverMessageId: -1
                                 }
                             }
@@ -448,7 +448,7 @@ async function startQasimDev() {
                             isForwarded: true,
                             forwardedNewsletterMessageInfo: {
                                 newsletterJid: '120363319098372999@newsletter',
-                                newsletterName: 'GlobalTechInc',
+                                newsletterName: 'HELLIOS',
                                 serverMessageId: -1
                             }
                         }
